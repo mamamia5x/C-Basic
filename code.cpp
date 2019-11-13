9c718cec7807a697377aaae775d49414b7ca336f
mamamia5x <57536929+mamamia5x@users.noreply.github.com>
2019-11-12 19:58:39 -0600

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include "code.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void usercode()
+{
+  system ("clear");
+  cout << "You didn't make anything." << endl;
+}
+
+
+
+//Copy and paste the code above to code.cpp to have it set to the default code when you start the game.
